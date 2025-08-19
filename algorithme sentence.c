45b902a2e38c78290536f05bce89ec3d0b6c64e8
2ayoubb <amalmalika088@gmail.com>
2025-08-19 10:39:15 +0100

    final commit

---
+++ algorithme sentence.c
@@ -0,0 +1,18 @@
+ALGORITHM sentence
+var 
+int longeur ; int vowels ;
+int i ;
+int j ;
+begin
+printf("ecrire la longeur ");
+longeur= 0
+for (i=0 ; i<j ; i++ ){
+    longeur = longeur +1 ;
+}
+vowels = 0
+if ( "a,o,y,i,e,u " ){
+    vowels = vowels +1 ;
+}
+
+ printf("la longeur est :" = longeur );
+  printf(" vowels est :" = vowels );
